f03da2efad605ccc30b612c62027faeef3fa1866
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-05-20 08:41:43 +0200

    Primitive types: bools

--- syntax/primitive_types.rs
+++ syntax/primitive_types.rs
@@ -1,9 +1,12 @@
 fn main() {
-    let x = true
-    let y: bool = false
+    let x = true;
+    let y: bool = false;
 
     if x {
         println!("x is {}", x);
+    }
+
+    if !y {
         println!("y is {}", y);
     }
 }
