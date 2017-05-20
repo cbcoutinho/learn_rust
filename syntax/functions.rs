d88c017982434d28bcf758e7627ea6294621915e
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-05-20 08:13:06 +0200

    basic functions

---
+++ syntax/functions.rs
@@ -0,0 +1,22 @@
+fn main() {
+    print_number(5);
+    print_sum(5, 6);
+    print_number(add_one(2));
+    // diverges();
+}
+
+fn print_number(x: i32) {
+    println!("x is: {}", x);
+}
+
+fn print_sum(x: i32, y: i32) {
+    println!("sum is: {}", x + y);
+}
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+// fn diverges() -> ! {
+//     panic!("This function never returns!");
+// }
