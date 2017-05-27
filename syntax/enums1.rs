f2f03993cc44db497831d50393bd0bddd4689f29
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-05-28 00:19:28 +0200

    add structs and enums

---
+++ syntax/enums1.rs
@@ -0,0 +1,21 @@
+enum Message {
+    Quit,
+    ChangeColor(i32, i32, i32),
+    Move { x: i32, y: i32 },
+    Write(String),
+}
+
+fn main() {
+    let x: Message = Message::Move { x: 3, y: 4 };
+
+    enum BoardGameTurn {
+        Move { squares: i32 },
+        Pass,
+    }
+
+    let y: BoardGameTurn = BoardGameTurn::Move { squares: 1 };
+
+    let m = Message::Write("Hello, world".to_string());
+
+    println!("My Message: {}", m.Write());
+}
