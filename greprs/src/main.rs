1e0b98d00a21fc5aef3e303520c1c756734303a6
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-07-16 17:52:33 +0200

    Move logic into lib.rs

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -1,7 +1,9 @@
+extern crate greprs;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+
+use greprs::Config;
 
 fn main() {
     // Collect command-line args into a vector of strings
@@ -15,29 +17,10 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("File not found");
-    let mut contents = String::new();
+    if let Err(e) = greprs::run(config) {
+        println!("Application error: {}", e);
 
-    f.read_to_string(&mut contents).expect("something went wrong reading the file");
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough input arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {
-            query: query,
-            filename: filename,
-        })
+        process::exit(1);
     }
+
 }
