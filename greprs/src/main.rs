6af7bbbb7e9a8191065b0d6e4a36f81808fb17c8
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-07-16 17:20:21 +0200

    Make error message more user friendly

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -1,12 +1,16 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     // Collect command-line args into a vector of strings
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problems parsing arguments: {}", err);
+        process::exit(1)
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -24,13 +28,16 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough input arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config {
+        Ok(Config {
             query: query,
             filename: filename,
-        }
+        })
     }
 }
