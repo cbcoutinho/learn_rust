14cd42e42c2c05acc056f3ad46ef13de383ba067
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-07-16 18:35:23 +0200

    Move error messaging to stderr instead of stdout

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -2,24 +2,31 @@ extern crate greprs;
 
 use std::env;
 use std::process;
+use std::io::prelude::*;
 
 use greprs::Config;
 
 fn main() {
     // Collect command-line args into a vector of strings
     let args: Vec<String> = env::args().collect();
+    let mut stderr = std::io::stderr();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problems parsing arguments: {}", err);
+        writeln!(
+            &mut stderr,
+            "Problems parsing arguments: {}",
+            err
+        ).expect("Could not write to stderr");
         process::exit(1)
     });
 
-    // println!("Searching for {}", config.query);
-    // println!("In file {}", config.filename);
 
     if let Err(e) = greprs::run(config) {
-        println!("Application error: {}", e);
-
+        writeln!(
+            &mut stderr,
+            "Application error: {}",
+            e
+        ).expect("Could not write to stderr");
         process::exit(1);
     }
 
