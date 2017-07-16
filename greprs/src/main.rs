1b24448d41671fcd5670e995804f8a2167cb0e57
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-07-16 17:08:39 +0200

    Move args into a Config struct

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -6,17 +6,31 @@ fn main() {
     // Collect command-line args into a vector of strings
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("File not found");
-
+    let mut f = File::open(config.filename).expect("File not found");
     let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("something went wrong reading the file");
 
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config {
+        query: query,
+        filename: filename,
+    }
 
 }
