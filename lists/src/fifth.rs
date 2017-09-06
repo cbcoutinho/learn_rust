69ccc321464efe74e7fdd308481c2f4ad32e72de
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-09-06 21:31:31 +0200

    Add pop as well as a test

--- lists/src/fifth.rs
+++ lists/src/fifth.rs
@@ -39,26 +39,24 @@ impl<T> List<T> {
         self.tail = raw_tail;
     }
 
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|head| {
+            let head = *head;
+            self.head = head.next;
 
-    // pub fn pop(&'a mut self) -> Option<T> {
-    //     // Grab the list's current head
-    //     self.head.take().map(|head| {
-    //         let head = *head;
-    //         self.head = head.next;
-    //
-    //         // If we're out of `head`, make sure to set the tail to `None`.
-    //         if self.head.is_none() {
-    //             self.tail = None;
-    //         }
-    //
-    //         head.elem
-    //     })
-    // }
+            if self.head.is_none() {
+                self.tail = ptr::null_mut();
+            }
+
+            head.elem
+        })
+    }
 }
 
+#[cfg(test)]
 mod test {
     use super::List;
-
+    
     #[test]
     fn basics() {
         let mut list = List::new();
@@ -85,6 +83,15 @@ mod test {
 
         // Check exhaustion
         assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        // Check the exhaustion case fixed the pointer right
+        list.push(6);
+        list.push(7);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
         assert_eq!(list.pop(), None);
     }
 }
