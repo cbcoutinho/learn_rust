5e838de89b6a61e09072b00949fa801c39b8a180
cbcoutinho <chrisbcoutinho@gmail.com>
2018-03-04 11:57:56 +0100

    Add a `String` read function with input

--- hello_world/src/hello/mod.rs
+++ hello_world/src/hello/mod.rs
@@ -1,6 +1,14 @@
+use std::io::stdin;
+
 pub fn test() {
     println!("Hello world!");
 
     let x = ::rand::random::<u8>();
     println!("{}", x);
+}
+
+pub fn read_name() -> String {
+    let mut name = String::new();
+    stdin().read_line(&mut name);
+    name
 }
--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,6 +1,11 @@
 extern crate rand;
+
 mod hello;
 
 fn main() {
-    hello::test();
+    let name = hello::read_name();
+    //let name2 = name;
+
+    println!("name: {}", name); // <- Move error, name2
+                                //println!("name2: {}", name2);
 }
