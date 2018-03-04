6116cecaaeb37cb9c84ed4dacdab531b2b9cd1cb
cbcoutinho <chrisbcoutinho@gmail.com>
2018-03-04 11:45:59 +0100

    Use rustfmt on lists/src/fourth.rs

--- lists/src/fourth.rs
+++ lists/src/fourth.rs
@@ -39,33 +39,35 @@ impl<T> List<T> {
             Some(old_head) => {
                 // non-empty list, need to connect the old head
                 old_head.borrow_mut().prev = Some(new_head.clone()); // +1 new_head
-                new_head.borrow_mut().next = Some(old_head);         // +1 old_head
-                self.head = Some(new_head);             // +1 new_head, -1 old_head
-                // total: +2 new_head, +0 old_head
+                new_head.borrow_mut().next = Some(old_head); // +1 old_head
+                self.head = Some(new_head); // +1 new_head, -1 old_head
+                                            // total: +2 new_head, +0 old_head
             }
             None => {
                 // emtpy list, need to set the tail
-                self.tail = Some(new_head.clone());     // +1 new_head
-                self.head = Some(new_head);             // +1 new_head
-                // total: +2 new_head
+                self.tail = Some(new_head.clone()); // +1 new_head
+                self.head = Some(new_head); // +1 new_head
+                                            // total: +2 new_head
             }
         }
     }
 
     pub fn pop_front(&mut self) -> Option<T> {
         // need to take the old head, ensuring it's -2
-        self.head.take().map(|old_head| {               // -1 old
+        self.head.take().map(|old_head| {
+            // -1 old
             match old_head.borrow_mut().next.take() {
-                Some(new_head) => {                     // -1 new
+                Some(new_head) => {
+                    // -1 new
                     // not emptying list
-                    new_head.borrow_mut().prev.take();  // -1 old
-                    self.head = Some(new_head);         // +1 new
-                    // total: -2 old, +0 new
+                    new_head.borrow_mut().prev.take(); // -1 old
+                    self.head = Some(new_head); // +1 new
+                                                // total: -2 old, +0 new
                 }
                 None => {
                     // emptying list
-                    self.tail.take();                   // -1 old
-                    // total: -2 old, (no new)
+                    self.tail.take(); // -1 old
+                                      // total: -2 old, (no new)
                 }
             }
             Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
