9e93e7acd0affc5014077f3e5bc56f51d4687300
Chris Coutinho <chrisbcoutinho@gmail.com>
2018-03-31 14:51:59 +0200

    Use auto-generated version and author info macros

--- par/src/main.rs
+++ par/src/main.rs
@@ -1,11 +1,14 @@
+#[macro_use]
 extern crate clap;
+
 use clap::App;
 
 fn main() {
     let _matches = App::new("par")
-        .version("0.1.0")
-        .about("Text (re)formatter")
-        .author("Chris Coutinho")
+        .version(crate_version!())
+        //.about("Text (re)formatter")
+        .about(crate_description!())
+        .author(crate_authors!())
         .get_matches();
 
     println!("Hello World!")
